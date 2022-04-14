4b5fc0c9a33432e854ac506b117c9249939deb5f
Kunj-Choksi <kunj07soni@gmail.com>
2022-04-14 12:55:26 -0400

    params checking methos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,13 @@ class ApplicationController < ActionController::Base
     render json: obj
   end
 
-  def has_sufficient_params api_params
-    
+  def has_sufficient_params(api_params)
+    api_params.each do |key|
+      unless params[key].present?
+        render_error_message '${key} not present!'
+        return false
+      end
+    end
+    true
   end
 end
