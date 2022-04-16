2b56c1eb050fb3897a5edca25a70cead7d481a1e
Karan Parikh <karanparikh@Karans-MacBook-Air.local>
2022-04-16 15:09:56 +0530

    indent fixing

--- app/controllers/daily_accounts_controller.rb
+++ app/controllers/daily_accounts_controller.rb
@@ -1,21 +1,22 @@
 class DailyAccountsController < ApplicationController
 
 	def get_daily_accounts
-    render_result_json DailyAccount.all
-  end
-
-  def update_daily_account
-    return unless has_sufficient_params(%w[id capacity])
-
-    daily_account = DailyAccount.where(id: params[:id]).first
-
-    unless daily_account.present?
-      render_error_message 'Account info not found!'
-      return
+        render_result_json DailyAccount.all
     end
 
-    daily_account.update(capacity: params[:capacity])
+    def update_daily_account
+        return unless has_sufficient_params(%w[id capacity])
 
-    render_result_message 'Updated successfully!'
-  end
+        daily_account = DailyAccount.where(id: params[:id]).first
+
+        unless daily_account.present?
+            render_error_message 'Account info not found!'
+            return
+        end
+
+        daily_account.update(capacity: params[:capacity])
+
+        render_result_message 'Updated successfully!'
+    end
+
 end
